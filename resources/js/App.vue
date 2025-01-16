7f02ff3bf49d0ab784d6a2d7d8b8476cd3d24db9
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-01-16 18:49:20 -0300

    Php serve funciona OK

---
+++ resources/js/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center">
+    <h1 class="text-3xl font-bold">Aca ira el login y registro</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+/* Aquí puedes agregar estilos específicos para este componente */
+</style>
