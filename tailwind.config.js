bd95e0f7cb98e5d1fae1c6e6d4e92e3eac605a95
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-01-16 18:53:29 -0300

    Instalacion de tailwind y cambio de create user migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,12 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/**/*.blade.php',
-        './resources/**/*.js',
-        './resources/**/*.vue',
-    ],
+// tailwind.config.js
+module.exports = {
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+      extend: {},
+    },
+    variants: {
+      extend: {},
     },
     plugins: [],
-};
+  }
